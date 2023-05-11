59bb501074402c35efbd59b01bbad070126f99db
Katia <katia@linux.com>
2023-05-11 22:42:43 +0200

    openresty: part 3.2

--- lua/inject_meta.lua
+++ lua/inject_meta.lua
@@ -38,7 +38,7 @@ if icao then
     end
 
     local description = "Check out this aircraft."
-    local groundspeed = -1;
+    local max_groundspeed = -1;
     local min_lat, min_lon, max_lat, max_lon = false, false, false, false
     if res and res.status == 200 then
         local cjson = require "cjson"
@@ -52,45 +52,36 @@ if icao then
                 local reg = aircraft["r"] or ""
                 local reg_country = find_country_by_hex(aircraft["hex"] or "")
                 reg = reg_country .. reg
-                -- if i == 1 then
-                --     -- get where we are flying from https://nominatim.openstreetmap.org/reverse?latx&lon=y&zoom=5&format=json
-                --     -- and then get .address.country, .address.state
-                --     res, err = httpc:request_uri("https://nominatim.openstreetmap.org/reverse?lat=" .. aircraft["lat"] .. "&lon=" .. aircraft["lon"] .. "&zoom=5&format=json", {
-                --         ssl_verify = false
-                --     })
-                --     if res and res.status == 200 then
-                --         local data = cjson.decode(res.body)
-                --         local country = data["address"]["country"] or ""
-                --         local state = data["address"]["state"] or ""
-                --         -- separator is ", " if both country and state are present, otherwise use ""
-                --         local separator = country ~= "" and state ~= "" and ", " or ""
-                --         -- check that iether country or state are set
-                --         if country ~= "" or state ~= "" then
-                --             table.insert(aircraft_descriptions, string.format("✈️🗺️🔎 %s%s%s", country, separator, state))
-                --         end
-                --     end
-                local tentative_gs = aircraft["gs"] or -1
-                if tentative_gs > groundspeed then
-                    groundspeed = tentative_gs
+
+                aircraft["lat"] = tonumber(aircraft["lat"]) or false
+                aircraft["lon"] = tonumber(aircraft["lon"]) or false
+
+                local tentative_gs = tonumber(aircraft["gs"]) or -1
+                if tentative_gs > max_groundspeed then
+                    max_groundspeed = tentative_gs
                 end
+
+                if aircraft["lat"] and aircraft["lon"] then
+                    if not min_lat or not min_lon or not max_lat or not max_lon then
+                        ngx.log(ngx.ERR, "first valid aircraft")
+                        min_lat = aircraft["lat"]
+                        min_lon = aircraft["lon"]
+                        max_lat = aircraft["lat"]
+                        max_lon = aircraft["lon"]
+                    else
+                        ngx.log(ngx.ERR, "not first aircraft")
+                        min_lat = math.min(min_lat, aircraft["lat"])
+                        min_lon = math.min(min_lon, aircraft["lon"])
+                        max_lat = math.max(max_lat, aircraft["lat"])
+                        max_lon = math.max(max_lon, aircraft["lon"])
+                    end
+                else
+                    ngx.log(ngx.ERR, "invalid lat/lon for aircraft ", aircraft["hex"])
+                end
+
                 ngx.log(ngx.ERR, "groundspeed: ", groundspeed)
                 ngx.log(ngx.ERR, "ac lat: ", aircraft["lat"])
                 ngx.log(ngx.ERR, "ac lon: ", aircraft["lon"])
-                -- tonumber
-                aircraft["lat"] = tonumber(aircraft["lat"])
-                aircraft["lon"] = tonumber(aircraft["lon"])
-                if not min_lat or aircraft["lat"] < min_lat then
-                    min_lat = aircraft["lat"]
-                end
-                if not min_lon or aircraft["lon"] < min_lon then
-                    min_lon = aircraft["lon"]
-                end
-                if not max_lat or aircraft["lat"] > max_lat then
-                    max_lat = aircraft["lat"]
-                end
-                if not max_lon or aircraft["lon"] > max_lon then
-                    max_lon = aircraft["lon"]
-                end
 
 
                 local type = aircraft["t"] or ""
@@ -144,14 +135,15 @@ if icao then
     local cache_key = icao
 
     local image_url = "https://api-dev.adsb.lol/0/screenshot/" .. icao .. "?"
-    if groundspeed > 0 then
-        image_url = image_url .. "gs=" .. groundspeed .. "&"
+    if max_groundspeed > 0 then
+        image_url = image_url .. "gs=" .. max_groundspeed .. "&"
     end
     if min_lat and min_lon and max_lat and max_lon then
         image_url = image_url .. "min_lat=" .. min_lat .. "&min_lon=" .. min_lon .. "&max_lat=" .. max_lat .. "&max_lon=" .. max_lon .. "&"
     end
     ngx.log(ngx.ERR, "lat/lons: ", min_lat, min_lon, max_lat, max_lon)
-    open_graph_tags:set(cache_key .. ":image", image_url)
+    -- remove image_url trailing & if found
+    open_graph_tags:set(cache_key .. ":image", image_url:gsub("&$", ""))
     open_graph_tags:set(cache_key .. ":description", description)
 else
     ngx.log(ngx.ERR, "No icao parameter in URL")
