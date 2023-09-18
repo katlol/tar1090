9f50f502124d57ae99ed00271c36f133e554be39
Katia Esposito <katia@linux.com>
2023-09-18 16:01:01 +0200

    adsb.lol add B612

--- html/script.js
+++ html/script.js
@@ -8323,7 +8323,7 @@ function setSelectedIcao() {
         return;
     }
     selIcao = selected.icao;
-    let hex_html = "<span style='font-family: monospace;' class=identSmall>Hex:" + NBSP + selected.icao.toUpperCase() + "</span>";
+    let hex_html = "<span style='font-family: 'B612 Mono', monospace;' class=identSmall>Hex:" + NBSP + selected.icao.toUpperCase() + "</span>";
     if (globeIndex || shareBaseUrl) {
         if (copiedIcao && (copiedIcao != selected.icao || new Date().getTime() - copyLinkTime > 2000)) {
             copiedIcao = null;
